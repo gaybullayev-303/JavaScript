fca87527f17b6c1c06d4e2a19ccdacdb974b6645
gaybullayev-303 <hanifaqosimova701@gmail.com>
2025-10-17 17:40:51 +0500

    4-dars

--- 4-dars/index.js
+++ 4-dars/index.js
@@ -1,2 +1,29 @@
-let = prompt()
-console.log(name + a)
+// data type - ma'lumot turi
+// 
+
+
+// 1. Primitive :
+// 1.1 - string : "ali" '324342'   `salom`
+// 1.2 - number : +- 2**53,  1,2,3,4,5,6,7,8,9        (biz biladigan tasavur qila oladigan raqamlar)
+// 1.3 - bigint : 900719924740993n = 2**53 + 1
+// 1.4 - boolean : true, false
+// 1.5 - symbol : bir-biriga oxshamas data yasab beradi
+// 1.6 - null : yoq malumot                                  (ozimiz kiritamiz)
+// 1.7 - undefined : yoq malumot                        (kompyuterni ozi chiqarib beradi)
+
+// ============================================================================================================================
+
+
+// 2. Non primitive 
+// object --> object, array, function 
+
+
+// 2.1 - object : {key: value , key2 : value2}
+// let data = {name: ali , age: 20};
+
+// 2.2 - array : [hohlagan narsa yozish mumkin!]
+
+// 2.3 - function :
+//  1. function xisobla (){}
+// 2. let xisobla2 = function(){}
+// 3. let xisobla3 = ()=>{}
