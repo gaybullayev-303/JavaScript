85165f0eaf31138b02f6f7c6a00998e7969a4dd4
gaybullayev-303 <hanifaqosimova701@gmail.com>
2025-10-29 17:56:24 +0500

    8-dars

---
+++ 8-dars/index.js
@@ -0,0 +1,77 @@
+// TERNARY ---
+// let data = 21;
+
+
+// if()
+
+
+
+
+//=================================================
+
+// data % 2 === 1 
+//   ? console.log("toq son") 
+//   : data % 2 === 0 
+//   ? console.log("juft son")  
+//   : console.log("siz notogri qiymat kiritdingiz");
+
+// ----------------------------------------------------
+
+// continue -----
+
+// (agar bunda 5 yoki 6 ga kelganda ulash ishlamasin desangiz "continue" ishlatamiz)
+
+// for(let i = 1; i <= 10; i++){
+// if (i === 5 || i ===6)continue;
+// console.log(i);}
+
+// =======================================
+
+
+
+
+
+// for(let i = 1; i <= 10; i++){
+//     if(i = 1)console.log("bu toq son");
+        
+    
+//        if(i === 2)console.log("xto");
+        
+//         else if(i === 3)console.log("bu toq son");
+        
+//        else if(i === 4)console.log("bu juft son");
+        
+//        else if(i === 5)console.log("bu toq son");
+        
+//        else if(i === 6)console.log("bu juft son");
+        
+//        else if(i === 7)console.log("bu toq son");
+        
+//        else if(i === 8)console.log("bu juft son");
+        
+    
+//        else if(i === 9)console.log("bu toq son");
+        
+      
+//        else if(i === 10)console.log("bu juft son");
+        
+    
+// }
+
+
+// ----------------------------------------------
+
+// let sum_toq = 0
+// let sum_juft = 0
+
+// for(let i = 1; i <= 10; i++){
+//     if(i % 2){
+//         sum_toq = sum_toq + i
+//     }
+//     else{
+//         sum_juft = sum_juft + i
+//     }
+// }
+// console.log(sum_toq);
+// console.log(sum_juft);
+
