421f650e07f4720d4cecf7c0433e819f8ef0aa80
gaybullayev-303 <hanifaqosimova701@gmail.com>
2025-10-29 17:58:04 +0500

    7-dars

---
+++ 7-dars/index.js
@@ -0,0 +1,120 @@
+// let a = 7
+
+// else if (a % 2) {
+// console.log("toq son");
+// }
+
+//  else{
+//     console.log("juft son");
+// }
+// sonni toq yoki juft ekanligini bilish uchun shu formulani ishlatamiz.
+// /==================================================================================
+
+// Switch
+
+// let name = "eshmat"
+
+// switch (name) {
+//     case "eshmat" :
+//         console.log("ha name eshmatga teng");
+//         return ;
+//         case "toshmat" :
+//         console.log("ha name toshmatga teng");
+//         return ;
+//     }
+// ----------------------------------------------------------
+
+// let ism = prompt("yoshingizni kiriting")
+
+// switch(ism % 2){
+//     case 1 :
+//         console.log(ism * 20);
+//         break;
+//     default:
+//         console.log(ism * 10);    
+// }
+
+
+// let age = 3;
+
+// switch (age > 50){
+//     case age:
+//     console.log("kattalar");
+//     break;
+
+//     // default:
+//     //   console.log(kichiklar);
+      
+//     case age < 50:
+//     console.log("kichkinalar");
+//     break;
+// }
+
+
+  
+
+
+
+
+
+
+
+
+
+
+// let a = 5;
+// let b = 10;
+// let c = 30;
+
+// let sum = (a + b + c) / 3
+
+// switch(sum % 2){
+//     case 1:
+//         console.log("bu toq son");
+//         break;
+//     case 0: 
+//         console.log(juft);
+//         break;
+//     default:
+//         console.log("xato son kiritildi");
+        
+// }
+
+// let a = 1;
+// let b = 4;
+
+// let c = a + b < 4;
+
+// c = true ?? null
+
+// if(a + b < 4){
+//     console.log("true");
+
+// }
+// else {
+//     console.log("null");
+
+// }
+
+
+
+
+// let a = prompt('raqam kiriting')
+// switch (a) {
+//     case '0':
+//         alert("true");
+//         break;
+//     case '1':
+//         alert("false");
+//         break;
+//     case '2':
+//         alert("Ikki");
+//         break;
+//     case '3':
+//         alert("Uch");
+//         break;
+//     default:
+//         alert("Qiymat to'g'ri kelmadi.")
+// }
+
+
