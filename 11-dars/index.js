58d2c710c8501c7b1f882f1ffe72d9e01ea92852
gaybullayev-303 <hanifaqosimova701@gmail.com>
2025-11-05 17:49:41 +0500

    11-dars

---
+++ 11-dars/index.js
@@ -0,0 +1,16 @@
+// let a = 5
+// let b = 7
+// let c = 4
+// let d = 9
+
+// let number = function (sum1, sum2, sum3, sum4) {
+//     let sum = 0
+//     if ((a % 2) === 1) sum += a
+//     if ((b % 2) === 1) sum += b
+//     if ((c % 2) === 1) sum += c
+//     if ((d % 2) === 1) sum += d
+//     console.log(sum);
+
+
+// }
+// number(a, b, c, d);
