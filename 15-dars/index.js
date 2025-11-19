e77b91de611d8b76080dd33591a9cd7c9558872f
gaybullayev-303 <hanifaqosimova701@gmail.com>
2025-11-19 16:21:51 +0500

    15-dars

---
+++ 15-dars/index.js
@@ -0,0 +1,49 @@
+// let obj1 = {
+//     name: "eshmat",
+// }
+
+// let obj2 = {
+//     age: 20,
+// }
+
+// let bothobj = Object.assign(obj1, obj2)
+
+
+// let allobj = structuredClone(bothobj)
+
+// bothobj.age = 10;
+
+// console.log(bothobj);
+// console.log(allobj);
+
+// let parent = {
+//     name: "eshmat",
+//     age: 66,
+//     child: {
+//         name: "toshmat",
+//         age: 30,
+//         subchild: {
+//             name: "gulmat",
+//             age: 8
+//         }
+//     }
+// }
+// let {name , age, child: {name:ism, age:yosh, subchild: { name:name1, age:age1 } } } = parent;
+// console.log(name,age,ism,yosh,name1,age1);
+
+// let obj = {
+//     name:"eshmat",
+//     age :20,
+//     status:"student",
+//     address:"kasansay",
+//     ball:90
+// }
+
+// let sum = 0;
+
+// for(let a in obj){
+//     if(typeof obj[a] === "number"){
+//         sum += obj[a]
+//     }
+// }
+//         console.log(sum);
