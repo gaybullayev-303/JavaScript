93d8ea97882888128a0a4b7b3812307677477d4c
gaybullayev-303 <hanifaqosimova701@gmail.com>
2025-11-19 17:48:45 +0500

    16-dars

---
+++ 16-dars/index.js
@@ -0,0 +1,42 @@
+// numbers-------
+
+// let n1 = 10; --- intager / butun son
+// let n1 = 10.5; --- float / kasr son
+
+
+// let num = 20;
+// let num1 = new number("50");
+
+
+// console.log(n1,n2);
+// console.log(num , num1);
+// console.log(number(num));
+
+
+// let a = 20;
+// let b = number("50");
+// let c = new number("50");
+
+
+let a =" 100.05"
+
+// console.log(number(a));
+// console.log(parseint(a));
+// console.log(parsefoat(a));
+
+
+// 0 - 0000
+// 1 - 0001
+// 2 - 0010
+// 3 - 0011
+// 4 - 0100
+// 5 - 0101
+// 6 - 0110
+// 7 - 0111
+// 8 - 1000
+// 9 - 1001
+// 10 - 1010
+
+
+// let num1 = 10;
+// console.log(num1.toString(2));
