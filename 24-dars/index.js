5498cc16860bbda9f7a68a4df50eb0dbc8f890bd
gaybullayev-303 <hanifaqosimova701@gmail.com>
2025-12-24 17:12:13 +0500

    24-dars

---
+++ 24-dars/index.js
@@ -0,0 +1,36 @@
+// let arr = [
+//     {id: 1 , name : "eshmat"},
+//     {id: 2 , name : "gulmat"},
+//     {id: 3, name : "ali"},
+//     {id: 4, name : "vali"}
+// ]
+// arr = arr.filter((obj) => obj.name.includes("m"))
+// console.log(arr);
+
+// ----------------------------------------------
+
+// let arr = [
+//     {id: 1 , name : "eshmat", status:"student"},
+//     {id: 2 , name : "gulmat", status:"ishchi"},
+//     {id: 3, name : "ali", status:"quruvchi"},
+//     {id: 4, name : "vali", status:"ishsiz"},
+//     {id: 5, name : "toshmat", status:"teacher"}
+// ]
+// let arr2 = arr.sort((a,b) => a.name.localeCompare(b.name));
+//     console.log(arr2);
+// ----------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+// let arr = ['Algebra', 'History', 'Geometry', 'English']
+
+// // let arr2 = arr.sort((a,b) => a.localeCompare(b));
+// //     console.log(arr2);
+// let sorter = (textbooks) => textbooks.sort((a, b) => a.toLowerCase() > b.toLowerCase() ? 1 : -1)
