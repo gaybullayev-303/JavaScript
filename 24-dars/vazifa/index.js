bd1772b44b23d5544d8510fad3f83038a17490bd
gaybullayev-303 <hanifaqosimova701@gmail.com>
2025-12-25 21:03:15 +0500

    24-dars vazifasi

---
+++ 24-dars/vazifa/index.js
@@ -0,0 +1,37 @@
+// let people = [
+//     { id: 1, firstName: "Eshmat", lastName: "Toshmatov", age: 20, status: "Talaba" },
+//     { id: 2, firstName: "Gulmat", lastName: "Surmatov", age: 17, status: "O'quvchi" },
+//     { id: 3, firstName: "Dilshod", lastName: "Karimov", age: 25, status: "Ishchi" },
+//     { id: 4, firstName: "Malika", lastName: "Xoliqova", age: 30, status: "Uqituvchi" },
+//     { id: 5, firstName: "Zafar", lastName: "Bozorov", age: 19, status: "Talaba" },
+//     { id: 6, firstName: "Umida", lastName: "Raxmatova", age: 22, status: "Tadbirkor" },
+//     { id: 7, firstName: "Rustam", lastName: "Ziyodov", age: 35, status: "Dasturchi" },
+//     { id: 8, firstName: "Nodira", lastName: "Saidova", age: 28, status: "Muhandis" },
+//     { id: 9, firstName: "Javlon", lastName: "Qurbonov", age: 24, status: "Frilanser" },
+//     { id: 10, firstName: "Shahnoza", lastName: "Ergasheva", age: 18, status: "O'quvchi" }
+//   ];
+// 1 - masala 
+// let obj2 = {id: 5, firstName: "Ali", lastName: "Valiyev", age: 25, status: "Dasturchi"}
+// let first = people.find((obj) => obj.id === 5 ? console.log(obj = obj2) : console.log(obj));
+
+
+
+//  2 - masala
+// let second = people.filter((obj) => obj.lastName.includes("va"))
+// console.log(second);
+
+
+// 3 - masala
+// people = people.filter((obj) => obj.id !== 4 && obj.id !== 9);
+// console.log(people);
+
+//  4 - masala
+//   let id = people.find((obj) => obj.id === 7);
+//     if (id) {
+//         id.firstName = "Gulbashakar";
+// }
+// console.log(people);
+
+//   5 - masala
+// people = people.sort((a, b) => a.lastName.localeCompare(b.lastName))
+// console.log(people);
