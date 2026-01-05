0a5436ed09da67f4d281c4016af2eeb3c48f42ac
gaybullayev-303 <hanifaqosimova701@gmail.com>
2026-01-05 18:03:44 +0500

    26-dars

---
+++ 26-dars/index.js
@@ -0,0 +1,21 @@
+// let data = [
+//   { id: 1, ism: "Otabek", familiya: "Valiyev", yosh: 14 },
+//   { id: 2, ism: "Orifjon", familiya: "Abdujabborov", yosh: 15 },
+//   { id: 3, ism: "Asadbek", familiya: "Atamirzayev", yosh: 16 },
+//   { id: 4, ism: "Salimjon", familiya: "Qahramonov", yosh: 16 },
+//   { id: 5, ism: "Shoxjaxon", familiya: "G'aybullajonov", yosh: 17 }
+// ];
+
+// let edit = (yosh, key ,value)=>{
+//     return data.map((obj)=> (obj.yosh === yosh) ? obj[key] = value : obj);
+// }
+
+
+// edit(17, "status" , "talaba")
+// console.log(data);
+
+// let edit = (yosh , key , number)=>data.map((obj)=> (obj.yosh === yosh) ? (obj[key] /= number) : obj);
+
+
+// edit(16, 'yosh', 2)
+// console.log(data);
