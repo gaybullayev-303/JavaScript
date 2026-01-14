eb3c0a0c8cb6eaf37c9cdac3ee89d8c1519b8ece
gaybullayev-303 <hanifaqosimova701@gmail.com>
2026-01-14 17:39:56 +0500

    28-dars

---
+++ 28-dars/index.js
@@ -0,0 +1,11 @@
+// let map = new Map();
+// map.set("name" , "Ali");
+// map.set("age", 20);
+// console.log(map.get("name"));
+// console.log(map.has("name"));
+// console.log(map.size);
+// map.delete("age");
+
+// console.log(map)
+// map.clear();
+
