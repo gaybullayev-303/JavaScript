c52fd43de1110b2b7b64d2359816bbe23a4633d4
gaybullayev-303 <hanifaqosimova701@gmail.com>
2026-01-14 17:18:10 +0500

    27-dars

---
+++ 27-dars/index.js
@@ -0,0 +1,10 @@
+// let date = new Date();
+// // console.log(date.getTime());
+// // console.log(date.getDate());
+// console.log(date.toDateString()); //Wed Jan 14 2026
+// console.log(date.toTimeString()); //15:57:15 GMT+0500 (Узбекистан, стандартное время)
+// console.log(date.toUTCString()); //Wed, 14 Jan 2026 10:57:15 GMT
+// console.log(date.toLocaleString()); //14.01.2026, 15:57:15
+// console.log(date.toLocaleDateString()); // 14.01.2026
+// console.log(date.toLocaleTimeString()); // 15:57:15
+// console.log(date.toISOString()); //2026-01-14T10:58:55.618Z
