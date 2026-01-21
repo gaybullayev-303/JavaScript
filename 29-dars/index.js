4ed4ff3fb84cc9aa6a8dc0d38fafee504eebf6e4
gaybullayev-303 <hanifaqosimova701@gmail.com>
2026-01-21 17:30:12 +0500

    29-masala

---
+++ 29-dars/index.js
@@ -0,0 +1,13 @@
+// RECURSION FUNCTION - FUNKSIYA ICHIDA OSHA FUNKSIYANI CHAQIRISH !
+
+
+// let sum = 0;
+
+// function getsum(n1){
+//     if(n1){
+//     if(!(n1 % 15)) sum += n1
+//     getsum(n1 - 1)
+// }
+// }
+// getsum(100);
+// console.log(sum);
