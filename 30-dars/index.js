b5228e238ac2ca2187880b00aa98fff62cfe4959
gaybullayev-303 <hanifaqosimova701@gmail.com>
2026-01-25 20:30:38 +0500

    30-dars

---
+++ 30-dars/index.js
@@ -0,0 +1,28 @@
+// console.log(1);
+
+// setTimeout(()=>{
+//     console.log(2);
+    
+// },1000)
+
+// console.log(3);
+// ===============================================
+
+// setInterval(()=>{
+//     console.log("bu interval");
+    
+// },1000)
+// console.log(2);
+
+
+// --------------------------------------------------------
+
+
+// let n1 = 5;
+// let n2 = 7;
+// setTimeout(()=>{
+// console.log(a + b);
+// },5000)
+
+// let a = 5;
+// let b = n1 * n2;
