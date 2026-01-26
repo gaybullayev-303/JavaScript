865e24706c8d8e78ad2e6823f830762f97b23748
gaybullayev-303 <hanifaqosimova701@gmail.com>
2026-01-26 18:41:40 +0500

    31-dars

---
+++ 31-dars/index.js
@@ -0,0 +1,16 @@
+// let obj = {name:"ali" , age: 10};
+
+// valeu , writable , enumerable , configurable
+
+// Object.defineProperty(obj , "name" , {writable : false , enumerable : false , configurable: false}) ;
+
+// console.log(Object.values(obj));
+// console.log(Object.keys(obj));
+
+
+// delete obj.age;
+
+// console.log(Object.getOwnPropertyDescriptor(obj , "name"));
+
+// cloneobj = Object.getOwnPropertyDescriptors(obj);
+// console.log(cloneobj);
