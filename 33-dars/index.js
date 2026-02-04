811d02a69a494fbb7e316c79caa218f170828c8c
gaybullayev-303 <hanifaqosimova701@gmail.com>
2026-02-04 18:46:16 +0500

    33-dars

---
+++ 33-dars/index.js
@@ -0,0 +1,5 @@
+//let arr = [1,2,3,4,5,6,7,8,9,0]
+//arr.map((v)=> v * 2); // map da esa false bolsa ham qiymat qaytaradi !
+//arr = arr.filter((v)=> false) // filter birinchi value false yoki true ekanligini tekshiradi 
+// agar true bolsa qiymat qaytadi agar false bolsa bo'sh array qaytadi
+// console.log(arr);
