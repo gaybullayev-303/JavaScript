0d6da7772acc8f8f6d9a8888d12c583788c7c050
gaybullayev-303 <hanifaqosimova701@gmail.com>
2026-02-23 16:15:54 +0500

    35-dars

---
+++ 35-dars/index.js
@@ -0,0 +1,24 @@
+// function* generateID() {
+//     let i = 1 
+//     while (true) {
+//         yield i
+//         i++
+//     }
+
+// }
+
+// let obj = generateID()
+
+// let data = [
+//     {id : obj.next().value, name : "Ali"},
+//     {id : obj.next().value, name : "Ali"},
+//     {id : obj.next().value, name : "Ali"},
+//     {id : obj.next().value, name : "Ali"},
+//     {id : obj.next().value, name : "Ali"},
+//     {id : obj.next().value, name : "Ali"},
+//     {id : obj.next().value, name : "Ali"},
+//     {id : obj.next().value, name : "Ali"},
+//     {id : obj.next().value, name : "Ali"}
+// ]
+
+// console.log(data);
