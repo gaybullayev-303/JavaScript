6354c421142bf78b70869c1d8efbb0812bffd1a3
gaybullayev-303 <hanifaqosimova701@gmail.com>
2026-03-09 16:37:07 +0500

    37-dars

---
+++ 37-dars/index.js
@@ -0,0 +1,163 @@
+// let box = document.createElement("div");
+// box.style.width = "500px";
+// box.style.height = "300px";
+// box.style.border = "3px solid red";
+
+
+let body = document.querySelector("body");
+// body.append(box);
+
+// let h1 = document.createElement("h1");
+// h1.textContent = "Oxford";
+// box.append(h1);
+
+// h1.style.cssText = `
+// color: red;
+// text-align: center;
+// font-size: 55px;
+// `;
+
+// let inp = document.createElement("input");
+// inp.setAttribute("type", "password");
+// box.appendChild(inp);
+// ---------------------------------------------------------
+
+// html.style.fontFamily = "sans-serif";
+
+
+let card1 = document.createElement("div")
+let a = document.createElement("div")
+a.style.width = "100%"
+a.style.height = "300px"
+
+
+
+let rasm1 = document.createElement("h1")
+rasm1.textContent = "rasm1"
+a.append(rasm1);
+card1.append(a);
+
+
+let crd1 = document.createElement("h1")
+crd1.textContent = "card 1"
+card1.append(crd1);
+
+
+let p = document.createElement("p")
+p.textContent = "lorem ipsum dolar lorem ipsum dolar lorem ipsum dolar lorem ipsum dolar lorem ipsum dolar lorem ipsum dolar lorem ipsum"
+card1.append(p)
+
+let input = document.createElement("input")
+input.setAttribute("type", "button")
+input.style.width = "150px"
+input.style.height = "40px"
+input.style.borderRadius = "8px"
+card1.append(input);
+
+
+let inh1 = document.createElement("h2")
+inh1.textContent = "Read More"
+input.append(inh1);
+
+
+card1.style.height = "600px"
+card1.style.width = "400px"
+card1.style.background = "aquamarine";
+card1.style.borderRadius = "15px"
+body.append(card1);
+
+
+// =========================================================================
+
+
+
+
+// let card2 = document.createElement("div")
+// let a = document.createElement("div")
+// a.style.width = "100%"
+// a.style.height = "300px"
+
+
+
+// let rasm2 = document.createElement("h1")
+// rasm2.textContent = "rasm2"
+// a.append(rasm2);
+// card2.append(a);
+
+
+// let crd2 = document.createElement("h1")
+// crd2.textContent = "card 2"
+// card2.append(crd2);
+
+
+// let p = document.createElement("p")
+// p.textContent = "lorem ipsum dolar lorem ipsum dolar lorem ipsum dolar lorem ipsum dolar lorem ipsum dolar lorem ipsum dolar lorem ipsum"
+// card2.append(p)
+
+// let input = document.createElement("input")
+// input.setAttribute("type", "button")
+// input.style.width = "150px"
+// input.style.height = "40px"
+// input.style.borderRadius = "8px"
+// card2.append(input);
+
+
+// let inh1 = document.createElement("h2")
+// inh1.textContent = "Read More"
+// input.append(inh1);
+
+
+// card1.style.height = "600px"
+// card1.style.width = "400px"
+// card1.style.background = "aquamarine";
+// card1.style.borderRadius = "15px"
+// body.append(card2);
+
+
+// // ==============================================================================
+
+// let card3 = document.createElement("div")
+// let a = document.createElement("div")
+// a.style.width = "100%"
+// a.style.height = "300px"
+
+
+
+// let rasm3 = document.createElement("h1")
+// rasm1.textContent = "rasm 3"
+// a.append(rasm3);
+// card3.append(a);
+
+
+// let crd3 = document.createElement("h1")
+// crd3.textContent = "card 3"
+// card3.append(crd3);
+
+
+// let p = document.createElement("p")
+// p.textContent = "lorem ipsum dolar lorem ipsum dolar lorem ipsum dolar lorem ipsum dolar lorem ipsum dolar lorem ipsum dolar lorem ipsum"
+// card3.append(p)
+
+// let input = document.createElement("input")
+// input.setAttribute("type", "button")
+// input.style.width = "150px"
+// input.style.height = "40px"
+// input.style.borderRadius = "8px"
+// card3.append(input);
+
+
+// let inh1 = document.createElement("h2")
+// inh1.textContent = "Read More"
+// input.append(inh1);
+
+
+// card1.style.height = "600px"
+// card1.style.width = "400px"
+// card1.style.background = "aquamarine";
+// card1.style.borderRadius = "15px"
+// body.append(card3); 
+
+//  body.style.display = "flex"
+//   body.style.alignItems = "center"
+//  body.style.gap = "100px"
+// //  body.flex.direction = "column"
